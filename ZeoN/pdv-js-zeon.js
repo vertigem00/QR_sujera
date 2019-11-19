57c3cead5258bedbed195b05af940ad78d53b14e
Ale <alessandrokame@gmail.com>
2019-11-19 20:56:26 -0300

    versão0.9

---
+++ ZeoN/pdv-js-zeon.js
@@ -0,0 +1,24 @@
+
+function dialogo(event, option, tab) {
+    var i, contentDialog, dialoglinks, contentTab;
+
+    contentDialog = document.getElementsByClassName("containercontent");
+    for(i = 0; i < contentDialog.length; i++) {
+        contentDialog[i].style.display = "none";
+    }
+    dialoglinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < dialoglinks.length; i++) {
+        dialoglinks[i].className = dialoglinks[i].className.replace( "active", "");
+    }
+    document.getElementById(option).style.display = "block";
+    event.currentTarget.className += " active";
+
+    contentTab = document.getElementsByClassName("tabOption");
+
+    for(i = 0; i < contentTab.length; i++) {
+        contentTab[i].style.display = "none";
+    }
+    document.getElementById(tab).style.display = "block";
+
+
+}
